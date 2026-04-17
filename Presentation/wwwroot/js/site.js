0c8c39d0a63182eb139a4cf900e27fe3c0a9680e
Samuel Fehrm <samuel.fehrm@yh.nackademin.se>
2026-04-17 14:50:32 +0200

    add membership, customer service and about me UI

--- Presentation/wwwroot/js/site.js
+++ Presentation/wwwroot/js/site.js
@@ -1,1 +1,8 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const mobileBtn = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    mobileBtn?.addEventListener('click', function () {
+        mobileMenu?.classList.toggle('open');
+    });
+});
